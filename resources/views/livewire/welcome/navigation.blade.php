ece2939d977bf4b404fafa9ae1e12e37e83ad362
LeoneCarmelo <carmeloleone@outlook.it>
2024-04-02 18:57:08 +0200

    Add simple style on login and register

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,13 +1,13 @@
 <div class="d-flex justify-content-between align-items-center bg-dark p-2">
-    <div class="logo text-light">Ecommerce-logo</div>
+    <a href="{{ url('/') }}" class="logo text-light text-decoration-none">Ecommerce-logo</a>
     @auth
         <a href="{{ url('/dashboard') }}" class="text-decoration-none" wire:navigate>Dashboard</a>
     @else
     <div class="">
-        <a href="{{ route('login') }}" class="text-decoration-none mx-2 text-light" wire:navigate>Log in</a>
+        <a href="{{ route('login') }}" class="text-decoration-none mx-2 text-light {{ Route::currentRouteName() === 'login' ? 'd-none' : '' }}"  wire:navigate>Log in</a>
 
         @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="text-decoration-none mx-2 text-light" wire:navigate>Register</a>
+            <a href="{{ route('register') }}" class="text-decoration-none mx-2 text-light {{ Route::currentRouteName() === 'register' ? 'd-none' : '' }}" wire:navigate>Register</a>
         @endif
     </div>
     @endauth
